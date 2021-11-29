05d84c710d34dbb8039db6121bbbc1915040571e
Javier <javier@eskura21.com>
2021-11-29 19:48:42 +0100

    i18n de números y fechas y datos en componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListadoComponent } from './listado/listado.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
@@ -16,7 +20,7 @@ import { FormularioComponent } from './formulario/formulario.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'es-ES' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/listado/listado.component.ts
+++ src/app/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from '../producto';
 
 @Component({
   selector: 'app-listado',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent implements OnInit {
-
+  productos: Producto[] = [
+    { id: 1, nombre: 'Portátil', precio: 321.45, fechaCaducidad: new Date() },
+    { id: 2, nombre: 'Ratón', precio: 21.45, fechaCaducidad: new Date() },
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
