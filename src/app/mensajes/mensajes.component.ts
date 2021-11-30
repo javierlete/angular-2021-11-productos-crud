f65131b9450576ad182d06516156be6def3b8192
Javier <javier@eskura21.com>
2021-11-30 19:07:21 +0100

    Componente de mensajes

--- src/app/mensajes/mensajes.component.ts
+++ src/app/mensajes/mensajes.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MensajeroService } from '../mensajero.service';
 
 @Component({
   selector: 'app-mensajes',
   templateUrl: './mensajes.component.html',
   styleUrls: ['./mensajes.component.css']
 })
-export class MensajesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MensajesComponent {
+  constructor(public mensajero: MensajeroService) { }
 }
